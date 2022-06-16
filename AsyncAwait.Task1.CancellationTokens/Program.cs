04c4ba129cbdb6835be89ad00eb8e80ff55aa4b9
Viktoriia Gorneva <viktoriia.gorneva@ihsmarkit.com>
2022-06-16 19:57:20 +0300

    remove comments

--- AsyncAwait.Task1.CancellationTokens/Program.cs
+++ AsyncAwait.Task1.CancellationTokens/Program.cs
@@ -53,65 +53,19 @@ internal class Program
 
     private static void CalculateSum(int n)
     {
-
-        if (CurrentTask.Item1 != null)
+        if (CurrentTask.Item1 != null && !CurrentTask.Item1.IsCompleted)
         {
             CurrentTask.Item2.Cancel(false);
         }
 
         var cancellationTokenSource = new CancellationTokenSource();
         var cancellationToken = cancellationTokenSource.Token;
-        var sum = 0;
 
-        // todo: make calculation asynchronous
 
         var calcTask = Task.Run(() => Calculator.Calculate(n, cancellationToken))
             .ContinueWith(x => PrintSum(n, x.Result), TaskContinuationOptions.NotOnCanceled);
 
         CurrentTask = (calcTask, cancellationTokenSource);
-
-        //var calculateSumTask = Task.Run(() => Calculator.Calculate(n, cancellationToken));
-        //var printSumTask = calculateSumTask.ContinueWith(x => PrintSum(n, calculateSumTask.Result), TaskContinuationOptions.NotOnCanceled);
-
-        ////var input = EnterN();
-        //if(input == "Q")
-        //{
-        //    return;
-        //}
-        //if (!calculateSumTask.IsCompleted)
-        //{
-        //    cancellationTokenSource.Cancel();
-        //    Console.WriteLine($"Sum for {n} cancelled...");
-        //    Console.WriteLine($"The task for {input} started... Enter N to cancel the request:");
-        //}
-        //// todo: add code to process cancellation and uncomment this line    
-
-        //CalculateSum(input);
-    }
-
-    static string EnterN()
-    {
-        var inputCorrect = false;
-        int newN = default;
-        Console.WriteLine("Enter N: ");
-
-        var input = Console.ReadLine();
-
-        while (!inputCorrect && input.ToUpper() != "Q")
-        {
-
-            inputCorrect = int.TryParse(input, out newN);
-            if (!inputCorrect)
-            {
-                Console.WriteLine($"Invalid integer: '{input}'. Please try again.");
-                Console.WriteLine("Enter N: ");
-
-                input = Console.ReadLine();
-            }
-
-        }
-
-        return input.ToUpper();
     }
 
     static void PrintSum(int n, long sum)
